171ed2ac3b75c4055ec602ff839f103f40b262ad
Clayton upf <claytonmagalhaes@upf.br>
2023-11-21 21:49:17 -0300

    Commit final

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ mudaServico();
 function mudaServico() {
     let servico = document.getElementById('selectServico').value;
     if (servico == 1) {
+        document.getElementById("divBuscaCEP").style.display = 'none';
         document.getElementById("divIMC").style.display = 'block';
         document.getElementById("divPorcento").style.display = 'none';
     } else if (servico == 2) {
+        document.getElementById("divBuscaCEP").style.display = 'none';
         document.getElementById("divIMC").style.display = 'none';
         document.getElementById("divPorcento").style.display = 'block';
+    } else if (servico == 4) {
+        document.getElementById("divBuscaCEP").style.display = 'block';
+        document.getElementById("divIMC").style.display = 'none';
+        document.getElementById("divPorcento").style.display = 'none';
     }
 }
 
@@ -25,4 +31,14 @@ function calculaPorcento() {
     let resultadoPorcento = (porcento*100) / valorTotal;
     resultadoPorcento = parseFloat(resultadoPorcento).toFixed(1);
     document.getElementById("resultadoPorcento").innerHTML = resultadoPorcento + " %";
+}
+
+async function buscaEndereco() {
+    let cep = document.getElementById("inputCEP").value;
+    const resposta = await fetch('https://brasilapi.com.br/api/cep/v1/'+cep);
+    const dados = await resposta.json();
+    console.log(dados);
+    console.log(dados['city']);
+    document.getElementById("pResultadoCEP").innerHTML = 
+        dados['city'] + '-' + dados['state'] + '-' + dados['street'];
 }
